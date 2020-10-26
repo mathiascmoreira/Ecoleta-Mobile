b5bd6f231052e96b9d886b6aa86481fff96813f1
Mathias <mathiascmoreira@gmail.com>
2020-10-25 22:58:55 -0300

    mobile app finished

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { AppLoading} from  'expo';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'; //O useFonts pode ser importado tando das fontes do ubunto quanto das fontes do roboto
+
+import Routes from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({  //As fontes utilizadas no projeto devem ser carregadas no inicio do projeto
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading /> //enquanto as fontes não forem carregadas, irá mostrar uma tela de carregamento; 
+  } 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
